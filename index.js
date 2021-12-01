973d53f0dcd930b1a42279fbb16db06da398f619
Zofia Janas <zoska91@wp.pl>
2021-12-01 19:08:23 +0100

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA74RObYqEU9irmM3dkdCXj1A5PDR_RfEg',
+  authDomain: 'meditateapp-69723.firebaseapp.com',
+  databaseURL: 'https://meditateapp-69723-default-rtdb.firebaseio.com',
+  projectId: 'meditateapp-69723',
+  storageBucket: 'meditateapp-69723.appspot.com',
+  messagingSenderId: '1038175316141',
+  appId: '1:1038175316141:web:fb5d6ffbb1c8d414427e24',
+  measurementId: 'G-LVEV36VSMK',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const email = input.value;
+  console.log(email);
+  createMeditation(email);
+});
+
+export const createMeditation = async email => {
+  db.collection('usersEmails')
+    .add({
+      email,
+    })
+    .then(() => {
+      console.log('Document successfully written!');
+      toastr.success(`Thank's! We have your email now!`);
+    })
+    .catch(error => {
+      console.error('Error writing document: ', error);
+    });
+};
